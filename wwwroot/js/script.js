aaaae02e6ec16aaabdee35af02fa8369cdc14a76
Lautaro Carro <luisb@lagash.com>
2019-10-17 16:13:55 -0300

    delete test button

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,17 +1,13 @@
 ﻿var form = document.getElementById('login');
-var buttonE1 = document.getElementById('e1');
 
 var urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has('loginresult') && urlParams.get('loginresult') === "false") {
-    form.classList.add('error_1');
-    setTimeout(function () {
-        form.classList.remove('error_1');
-    }, 3000);
+    showError()
 }
 
-buttonE1.addEventListener('click', function () {
+function showError() {
     form.classList.add('error_1');
     setTimeout(function () {
         form.classList.remove('error_1');
     }, 6000);
-});
+}
